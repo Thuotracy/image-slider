df4465456112872f0e2b02af74e7982492d2755f
Thuotracy <tracyjacobs775@gmail.com>
2022-07-25 16:14:09 +0300

    styled the buttons

--- main.js
+++ main.js
@@ -17,11 +17,17 @@ var i = 0;
 // next btn
 next.addEventListener('click', function(){
   i++;
+  if(i > arr.length - 1){
+    i = 0;
+  }
   document.getElementById('image').src = arr[i];
 })
 
 // prev btn
-// prev.addEventListener('click', function(){
-//     i--;
-//     document.getElementById('image').src = arr[i]; 
+prev.addEventListener('click', function(){
+    i--;
+    if(i < 0 ){
+      i = arr.length - 1;
+    }
+    document.getElementById('image').src = arr[i]; 
 })
