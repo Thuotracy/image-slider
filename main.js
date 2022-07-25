a025b9ff35794157e912bab32087e6e5a548768f
Thuotracy <tracyjacobs775@gmail.com>
2022-07-25 15:23:38 +0300

    added all the images into an array in main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+var prev = document.getElementById('previous');
+var next = document.getElementById('next');
+
+var arr = ["img/image1.jpg",
+           "img/image2.jpg", 
+           "img/image3.jpg", 
+           "img/image4.jpg", 
+           "img/image5.jpg", 
+           "img/image6.jpg", 
+           "img/image7.jpg",
+           "img/image8.jpg", 
+           "img/image9.jpg", 
+           "img/image10.jpg"]
